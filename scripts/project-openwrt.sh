ac229b9a7a3be0c259ab57e1b1d646d02c383a21
SuLingGG <1215009714@qq.com>
2020-08-05 11:59:47 +0800

    Test ipv6helper

---
+++ scripts/project-openwrt.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Remove r8168 driver
+rm -rf package/ctcgfw/r8168
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+# Add luci-app-jd-dailybonus
+git clone --depth=1 https://github.com/jerrykuku/node-request
+git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
+popd
+
+# Use latest luci-theme-argon
+rm -rf package/ctcgfw/luci-theme*
+rm -rf package/lean/luci-theme*
+rm -rf package/lienol/luci-theme*
+pushd package/community
+git clone -b 18.06 --depth=1 https://github.com/jerrykuku/luci-theme-argon
+popd
+
+# Mod zzz-default-settings
+sed -i "/commit luci/i\uci set luci.main.mediaurlbase='/luci-static/argon'" package/lean/default-settings/files/zzz-default-settings
+
+# Add po2lmo
+git clone https://github.com/openwrt-dev/po2lmo.git
+pushd po2lmo
+make && sudo make install
+popd
